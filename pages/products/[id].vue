d5cdecf434ce8a22f11b48862954e1930d34532c
JoannaTabbi <joanna.tabbi@gmail.com>
2023-01-30 21:44:43 +0100

    add dynamic route for product details

---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Product details for {{ id }}</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, soluta
+      itaque odit assumenda voluptatum deleniti doloribus.
+    </p>
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
