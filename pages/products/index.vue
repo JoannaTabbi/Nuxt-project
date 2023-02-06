47ece88686e0adf29185302dd7c1812df6292856
JoannaTabbi <joanna.tabbi@gmail.com>
2023-02-06 18:20:32 +0100

    fetch products collection

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-    <h2>Products</h2>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="product in products">
+       <NuxtLink :to="`/products/${product.id}`">{{ product.title }}</NuxtLink>
+      </div>
     </div>
 </template>
 
@@ -8,6 +10,9 @@
  definePageMeta({
     layout: 'products'
  })
+
+ // fetching products from fakestoreapi
+ const { data: products } = await useFetch('https://fakestoreapi.com/products')
 
 </script>
 
