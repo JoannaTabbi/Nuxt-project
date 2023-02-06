ca84c0885e66d3e703871e32da6fe0a4d69b352b
JoannaTabbi <joanna.tabbi@gmail.com>
2023-02-06 12:02:24 +0100

    add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li><NuxtLink to="/">Homepage</NuxtLink></li>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+            <li><NuxtLink to="/products">Products</NuxtLink></li>
+          </ul>
+        </nav>
+      </header>
+  
+      <!-- output page content  -->
+  
+      <div>
+          <slot />
+      </div>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style scoped>
+    .router-link-exact-active {
+      color: #12B488;
+    }
+  </style>
+  
