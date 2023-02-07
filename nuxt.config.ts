8aa7500d2447898514f31cc36459444b1e1a60aa
JoannaTabbi <joanna.tabbi@gmail.com>
2023-02-07 10:45:40 +0100

    set global meta data in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,16 @@ export default defineNuxtConfig({
   typescript: {
     shim: false
   },
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: 'Nuxt project',
+      meta: [
+        { name: 'description', content: 'Nuxt basic pratical exercice' }
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+      ]
+    }
+  }
 });
