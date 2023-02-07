b26b142815b4980bcd2953d5b2cda04de2c9dbd0
JoannaTabbi <joanna.tabbi@gmail.com>
2023-02-07 22:34:56 +0100

    api call with private key

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,41 @@
 <template>
   <div>
     <h2>Homepage</h2>
-    <div>{{ data.message }}</div>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
       officiis asperiores tempora, animi quas voluptate culpa molestias,
       molestiae dolorem ratione consectetur facilis ut laudantium iure rerum
       error itaque similique pariatur?
     </p>
+    <table class="table-auto">
+  <thead>
+    <tr>
+      <th>Currency code</th>
+      <th>Currency value</th>
+    </tr>
+  </thead>
+  <tbody v-for="currency in data">
+    <tr>
+      <td>{{ currency.code }}</td>
+      <td>{{ currency.value }}</td>
+    </tr>
+  </tbody>
+</table>
+
+
+    
   </div>
 </template>
 
 <script setup>
   // fetching data from internal server api
-  const { data } = await useFetch('/api/user?name=Mario', {
-    method: 'post',
-    body: { age : 30 }
-  })
+  //const { data } = await useFetch('/api/user?name=Mario', {
+  //  method: 'post',
+  //  body: { age : 30 }
+  //})
+  const { data } = await useFetch('/api/user')
+  
+
 </script>
 
 <style scoped>
@@ -26,5 +45,9 @@
   }
   p {
     margin: 20px 0;
+  }
+  th, td {
+    padding: 5px 10px 5px 5px;
+    border-bottom: 2px solid lightgrey;
   }
 </style>
--- server/api/user.js
+++ server/api/user.js
@@ -6,13 +6,18 @@
 export default defineEventHandler(async (event) => {
 
   // handle query params; getQuery replaces useQuery which is deprecated now
-   const { name } = getQuery(event)
+  // const { name } = getQuery(event)
 
   // handle post data 
-   const { age } = await readBody(event)
+  // const { age } = await readBody(event)
 
-  return {
-    message: `Welcome, ${name}, nice to meet you! You are ${age} years old`
-  }
+
+  // api call with private key
+     const { data } = await $fetch('https://api.currencyapi.com/v3/latest?apikey=9ewD8ya5tXg6D5IVu0a2nKJefkavu161sE4pWLnx&currencies=EUR%2CUSD%2CCAD')
+     
+  return data
+    //message: `Welcome, ${name}, nice to meet you! You are ${age} years old`
+    
+  
 
 })
